0037b44fc2d440d1aaad25a3f274d626642f7fc5
marchshares <marchshares@gmail.com>
2018-09-19 16:44:26 +0300

    commmit

---
+++ src/main/java/com/test/Converter.java
@@ -0,0 +1,84 @@
+package com.test;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Converter {
+    private static final Set<String> arrayNames = new HashSet<>();
+    static {
+        arrayNames.add("products");
+        arrayNames.add("options");
+    }
+
+    public JSONObject bodyLineToJsonObject(String bodyLine) {
+        JSONObject jsonObject = new JSONObject();
+
+        String[] params = bodyLine.split("&");
+
+        Arrays.stream(params).forEach(param -> {
+            String[] split = param.split("=", 2);
+
+            String name = split[0];
+            String value = split[1];
+
+            func(jsonObject, name, value);
+        });
+
+        convertArrays(jsonObject);
+
+        return jsonObject;
+    }
+
+    private void func(JSONObject jsonObject, String name, String value) {
+        if (! name.contains("[")) {
+            jsonObject.put(name, value);
+        } else {
+            String[] split1 = name.split("(\\[|\\])", 3);
+
+            String entryName = split1[0];
+            String fieldName = split1[1] + split1[2];
+
+            if (! jsonObject.has(entryName)) {
+                jsonObject.put(entryName, new JSONObject());
+            }
+
+            JSONObject entry = jsonObject.getJSONObject(entryName);
+
+            func(entry, fieldName, value);
+        }
+    }
+
+    private void convertArrays(JSONObject jsonObject) {
+        jsonObject.names().forEach(nameObj -> {
+            String name = (String) nameObj;
+            Object value = jsonObject.get(name);
+
+            if (value instanceof JSONObject) {
+                JSONObject jsonObjectValue = (JSONObject) value;
+
+                convertArrays(jsonObjectValue);
+
+                if (arrayNames.contains(name)) {
+                    jsonObject.put(name, convertEntryToArray(jsonObjectValue));
+                }
+            }
+        });
+    }
+
+    private static JSONArray convertEntryToArray(JSONObject entryArray) {
+        JSONArray jsonArray = new JSONArray();
+
+        entryArray.names()
+                .forEach(name -> {
+                    String idx = (String) name;
+                    jsonArray.put(Integer.parseInt(idx), entryArray.getJSONObject(idx));
+                });
+
+        return jsonArray;
+    }
+
+}
--- src/main/java/com/test/Order.java
+++ src/main/java/com/test/Order.java
@@ -2,44 +2,12 @@ package com.test;
 
 import org.json.JSONObject;
 
-import java.util.Arrays;
-
 public class Order {
 
     private final JSONObject jsonObject;
 
     public Order(String bodyLine) {
-        jsonObject = new JSONObject();
-
-        String[] params = bodyLine.split("&");
-
-        Arrays.stream(params).forEach(param -> {
-            String[] split = param.split("=", 2);
-
-            String name = split[0];
-            String value = split[1];
-
-            if (! name.contains("[")) {
-                jsonObject.put(name, value);
-            } else {
-                String[] split1 = param.split("(\\[|\\])", 3);
-
-                String entryName = split1[0];
-                String fieldName = split1[1];
-
-                String[] split2 = split1[2].split("=");
-                String fieldAddName = split2[0];
-                String fieldValue = split2[1];
-
-                if (! jsonObject.has(entryName)) {
-                    jsonObject.put(entryName, new JSONObject());
-                }
-                JSONObject entry = jsonObject.getJSONObject(entryName);
-
-                entry.put(fieldName+fieldAddName, fieldValue);
-            }
-        });
-
+        jsonObject = new Converter().bodyLineToJsonObject(bodyLine);
         System.out.println(jsonObject);
     }
 }
